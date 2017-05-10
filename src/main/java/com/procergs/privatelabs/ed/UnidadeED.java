965bcf8a04a5426e71e09502058c4781b4de5286
mariovalmir <mario.pereira@acad.pucrs.br>
2017-05-10 12:16:36 -0300

    Adicionado DataSource

---
+++ src/main/java/com/procergs/privatelabs/ed/UnidadeED.java
@@ -0,0 +1,24 @@
+package com.procergs.privatelabs.ed;
+
+public class UnidadeED {
+
+	
+	private Long idUnidade;
+	private String vendedor;
+
+	public Long getIdUnidade() {
+		return idUnidade;
+	}
+
+	public void setIdUnidade(Long idUnidade) {
+		this.idUnidade = idUnidade;
+	}
+
+	public String getVendedor() {
+		return vendedor;
+	}
+
+	public void setVendedor(String vendedor) {
+		this.vendedor = vendedor;
+	}
+}
